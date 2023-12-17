eb34e01295acdb37c972aed410054ea017ac5dcd
iitznee <154056983+iitznee@users.noreply.github.com>
2023-12-17 12:58:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['393515817062', 'NEE', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
